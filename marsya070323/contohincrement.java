47b4a8b31ac2fab6fa3e74bd9508e5eabd44ef55
Marsyaaa24 <marsyamissendhi24@gmail.com>
2023-03-16 10:28:14 +0700

    marsya070323

---
+++ marsya070323/contohincrement.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marsya070323;
+
+/**
+ *
+ * @author Marsya Missendhi
+ */
+public class contohincrement {
+    public static void main(String[] args){
+    int i = 10;
+    int j = 3;
+    int k = 0;
+    
+    k = ++j + i; //akan menghasilkan k = 4+10 = 14
+    
+    System.out.print(+k);
+    }
+    
+}
---
+++ marsya070323/hello.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marsya070323;
+
+/**
+ *
+ * @author Marsya Missendhi
+ */
+public class hello {
+    public static void main(String[] args){
+            System.out.println("hello marsyaaa");
+    }
+}
