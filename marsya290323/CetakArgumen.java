68bfe4615def5da438454ea1f52b054745214cdc
Marsyaaa24 <marsyamissendhi24@gmail.com>
2023-04-13 10:28:17 +0700

    marsya290323

---
+++ marsya290323/CetakArgumen.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marsya290323;
+
+/**
+ *
+ * @author Marsya Missendhi
+ */
+public class CetakArgumen {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
+
+
